a48a1e92f6903f680b7c561a117635658e35ceb7
MohammedAl-rowad <rowadwo@gmail.com>
2021-09-24 02:13:25 +0300

    Nice

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useRef } from 'react'
 import 'codemirror/lib/codemirror.css'
-import 'codemirror/lib/codemirror'
-// import 'codemirror/theme/material-ocean.css'
+// import 'codemirror/lib/codemirror'
+import 'codemirror/theme/material-ocean.css'
 import 'codemirror/mode/javascript/javascript'
+import 'codemirror/keymap/sublime'
 import CodeMirror from 'codemirror'
 import 'codemirror'
 
@@ -11,27 +12,34 @@ const App = () => {
   useEffect(() => {
     const editor = CodeMirror.fromTextArea(document.getElementById('ds'), {
       lineNumbers: true,
-      // theme: 'material-ocean',
-      // mode: 'javascript',
+      keyMap: 'sublime',
+      theme: 'material-ocean',
+      mode: 'javascript',
       // cursorBlinkRate: 1,
       // cursorWidth: 10,
       // height: '100vh',
     })
 
     console.log(editor.getAllMarks())
-    editor.setBookmark({ line: 1, ch: 1 })
+    const widget = document.createElement('span')
+    widget.textContent = 'hmmm?'
+    widget.style.cssText =
+      'background: #F37381; padding: 0px 3px; color: #F3F5F1; cursor: pointer;'
+
+    const bookMark = editor.setBookmark({ line: 1, pos: 1 }, { widget })
+    widget.onclick = () => bookMark.clear()
     console.log(editor.getAllMarks())
 
     editor.on('change', (instance, changes) => {
       // console.log(instance, changes)
-      console.log(1)
+      console.log(changes, instance.getValue())
     })
     editor.on('cursorActivity', (instance) => {
-      console.log(instance.cursorCoords())
+      // console.log(instance.cursorCoords())
     })
   }, [])
 
-  return <textarea id="ds" value="const a = 'rowad'"></textarea>
+  return <textarea id="ds" />
 }
 
 export default App
